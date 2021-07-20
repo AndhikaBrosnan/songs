add0c0386182b6c5812d248b4889d57bf5fa5724
andhikabrosnan <andhikabrosnan@gmail.com>
2021-07-20 22:56:28 +0700

    20thJuly2021

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,11 @@ import { combineReducers } from "redux";
 const songsReducers = () => {
   return [
     { title: "Hey Ya!", duration: "2:00" },
-    { title: "Hey Ya!", duration: "2:00" },
+    { title: "Smells Like Teen Spirit", duration: "4:40" },
     { title: "Bye Bye Bye", duration: "3:21" },
     { title: "Toxic", duration: "5:00" },
     { title: "Love Yourself", duration: "3:50" },
+    { title: "Wonderwall", duration: "4:39" },
   ];
 };
 
